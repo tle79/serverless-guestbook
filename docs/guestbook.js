3723961d60153761855f45766a0df01de4079116
tle79 <45411457+tle79@users.noreply.github.com>
2020-08-31 17:24:11 +0700

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://adbf3eab.eu-gb.apiconnect.appdomain.cloud/guestbook';
+const apiUrl = 'https://705073df.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
